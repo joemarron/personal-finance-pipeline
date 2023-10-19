07aba21e913ed4dbf724822fec53fcde2bfbd9a3
Joe Marron <joe.marron.99@gmail.com>
2023-10-19 12:40:33 +0100

    adjust DAG sql queries

--- airflow-docker/dags/mysql_scripts/map_temp_table.sql
+++ airflow-docker/dags/mysql_scripts/map_temp_table.sql
@@ -1,14 +1,14 @@
-INSERT INTO transactions (text_id, account_id, booking_date, description_text, amount, subcategory_id)
+INSERT INTO transactions (text_id, account_id, description_text, amount, subcategory_id, booking_date)
 SELECT 
-temp."transactionId",
-ac.account_id::FLOAT,
-temp."bookingDate",
-temp."remittanceInformationUnstructured",
+temp."transactionId"::STRING,
+ac.account_id::INT,
+temp."remittanceInformationUnstructured"::STRING,
 temp."transactionAmount.amount"::FLOAT,
-sm.subcategory_id::INTEGER  -- Subcategory mapping from the mapping table
-    
+sm.subcategory_id,  -- Subcategory mapping from the mapping table
+temp."bookingDate"::DATE
+
 FROM "#temp_transactions" AS temp
-LEFT JOIN accounts AS ac ON temp."Account" = ac.account
+LEFT JOIN accounts AS ac ON temp."Account" = CAST(ac.account AS STRING)
 LEFT JOIN "#mapping" sm ON temp."remittanceInformationUnstructured" = sm.description_text
 
 ON CONFLICT DO NOTHING;
